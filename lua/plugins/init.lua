3413bfe44122c39af6f65289b8dac5f9fd7f41bf
Shizukana Otaku <otakushizukana@gmail.com>
2024-11-18 17:37:56 +0000

    Add neorg

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -16,6 +16,32 @@ return {
   },
   { "lewis6991/gitsigns.nvim", opts = {} },
   {
+    {
+      "nvim-neorg/neorg",
+      lazy = false,
+      version = "*",
+      config = function()
+        require("neorg").setup {
+          load = {
+            ["core.defaults"] = {},
+            ["core.concealer"] = {},
+            ["core.dirman"] = {
+              config = {
+                workspaces = {
+                  notes = "~/notes",
+                },
+                default_workspace = "notes",
+              }
+            }
+          }
+        }
+
+        vim.wo.foldlevel = 99
+        vim.wo.conceallevel = 2
+      end,
+    }
+  },
+  {
     'saecki/crates.nvim',
     tag = 'stable',
     opts = {
