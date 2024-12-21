68d17f95ce9a42fb8bbbe4deab4da9afb591a173
Shizukana Otaku <otakushizukana@gmail.com>
2024-12-21 15:18:06 +0000

    Add zellij integration, tokyonight themes, clean them up

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,16 +1,7 @@
 return {
-  { "catppuccin/nvim",                 name = "catppuccin" },
-  { "yorickpeterse/happy_hacking.vim", name = "happy hacking" },
-  { "EdenEast/nightfox.nvim",          name = "Nightfox" },
-  { "rebelot/kanagawa.nvim",           name = "Kanagawa" },
-  {
-    "baliestri/aura-theme",
-    lazy = false,
-    priority = 1000,
-    config = function(plugin)
-      vim.opt.rtp:append(plugin.dir .. "/packages/neovim")
-      vim.cmd([[colorscheme aura-dark]])
-    end,
-    name = "Aura"
-  }
+  { "catppuccin/nvim",                 name = "Catppuccin theme" },
+  { "yorickpeterse/happy_hacking.vim", name = "Happy hacking theme" },
+  { "EdenEast/nightfox.nvim",          name = "Nightfox theme" },
+  { "rebelot/kanagawa.nvim",           name = "Kanagawa theme" },
+  { "folke/tokyonight.nvim",           name = "Tokyonight theme" },
 }
