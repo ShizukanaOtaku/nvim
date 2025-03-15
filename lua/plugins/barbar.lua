ca60345cccbf8ca8640dfb52e2f85f36c221845c
ShizukanaOtaku <otakushizukana@gmail.com>
2025-03-15 18:54:18 +0100

    tweaks to both

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -1,1 +1,7 @@
-return { "romgrk/barbar.nvim" }
+return {
+    "romgrk/barbar.nvim",
+    opts = {
+        animation = false,
+        insert_at_end = true
+    }
+}
--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -4,32 +4,32 @@ return {
     cmd = "Trouble",
     keys = {
         {
-            "<leader>cx",
+            "<leader>tx",
             "<cmd>Trouble diagnostics toggle<cr>",
             desc = "Diagnostics (Trouble)",
         },
         {
-            "<leader>cX",
+            "<leader>tX",
             "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
             desc = "Buffer Diagnostics (Trouble)",
         },
         {
-            "<leader>cs",
+            "<leader>ts",
             "<cmd>Trouble symbols toggle focus=false<cr>",
             desc = "Symbols (Trouble)",
         },
         {
-            "<leader>cl",
+            "<leader>tl",
             "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
             desc = "LSP Definitions / references / ... (Trouble)",
         },
         {
-            "<leader>cL",
+            "<leader>tL",
             "<cmd>Trouble loclist toggle<cr>",
             desc = "Location List (Trouble)",
         },
         {
-            "<leader>cQ",
+            "<leader>tQ",
             "<cmd>Trouble qflist toggle<cr>",
             desc = "Quickfix List (Trouble)",
         },
